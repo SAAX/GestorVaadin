e0c317123e21a558d94c4c9c09f09472675809a5
rodrigo <rodrigo.ccn2005@gmail.com>
2014-07-10 09:34:51 -0300

    Estruturas de Teste

--- GestorWeb/src/test/java/com/saax/gestorweb/dao/CentroCustoDAOTest.java
+++ GestorWeb/src/test/java/com/saax/gestorweb/dao/CentroCustoDAOTest.java
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.saax.gestorweb.dao;
 
+import com.saax.gestorweb.dao.exceptions.NonexistentEntityException;
 import com.saax.gestorweb.model.datamodel.CentroCusto;
 import com.saax.gestorweb.util.DBConnect;
+import com.saax.gestorweb.util.PostgresConnection;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,23 +26,23 @@ import static org.junit.Assert.*;
  * @author rodrigo
  */
 public class CentroCustoDAOTest {
-    
+
     public CentroCustoDAOTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
         DBConnect.getInstance().assertConnection();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -49,7 +53,7 @@ public class CentroCustoDAOTest {
     @Test
     public void testGetEntityManager() {
         System.out.println("getEntityManager");
-        CentroCustoDAO instance = null;
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
         EntityManager result = instance.getEntityManager();
         assertNotNull(result);
     }
@@ -60,11 +64,29 @@ public class CentroCustoDAOTest {
     @Test
     public void testCreate() {
         System.out.println("create");
-        CentroCusto centroCusto = null;
-        CentroCustoDAO instance = null;
+
+        // Teste #1 - sucesso
+        CentroCusto centroCusto = new CentroCusto();
+        centroCusto.setCentroCusto("NomeCC");
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
         instance.create(centroCusto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        assertEquals(instance.findCentroCusto(centroCusto.getId()), centroCusto);
+
+        try {
+            instance.destroy(centroCusto.getId());
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
+        // Teste #2 - falha
+        try {
+            instance.create(null);
+            fail("Exceção esperada");
+        } catch (Exception ex) {
+        }
+
     }
 
     /**
@@ -72,12 +94,36 @@ public class CentroCustoDAOTest {
      */
     @Test
     public void testEdit() throws Exception {
+
         System.out.println("edit");
-        CentroCusto centroCusto = null;
-        CentroCustoDAO instance = null;
+
+        // Teste #1 - sucesso
+        CentroCusto centroCusto = new CentroCusto();
+        centroCusto.setCentroCusto("NomeCC");
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+        instance.create(centroCusto);
+
+        String expResult = "Nome CC 2";
+        centroCusto.setCentroCusto(expResult);
         instance.edit(centroCusto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        String result = instance.findCentroCusto(centroCusto.getId()).getCentroCusto();
+        assertEquals(expResult, result);
+
+        try {
+            instance.destroy(centroCusto.getId());
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
+        // Teste #2 - falha
+        try {
+            instance.edit(new CentroCusto());
+            fail("Exceção esperada");
+        } catch (Exception ex) {
+        }
+
     }
 
     /**
@@ -86,11 +132,28 @@ public class CentroCustoDAOTest {
     @Test
     public void testDestroy() throws Exception {
         System.out.println("destroy");
-        Integer id = null;
-        CentroCustoDAO instance = null;
-        instance.destroy(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        CentroCusto centroCusto = new CentroCusto();
+        centroCusto.setCentroCusto("NomeCC");
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+        instance.create(centroCusto);
+
+        try {
+            instance.destroy(centroCusto.getId());
+
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
+        // tenta remover novamente o mesmo elemento
+        // deve lançar exceção
+        try {
+            instance.destroy(centroCusto.getId());
+            fail("Exceção esperada");
+        } catch (NonexistentEntityException ex) {
+        }
+
     }
 
     /**
@@ -99,12 +162,45 @@ public class CentroCustoDAOTest {
     @Test
     public void testFindCentroCustoEntities_0args() {
         System.out.println("findCentroCustoEntities");
-        CentroCustoDAO instance = null;
-        List<CentroCusto> expResult = null;
+
+        List<CentroCusto> expResult = new ArrayList<>();
+
+        expResult.add(new CentroCusto());
+        expResult.add(new CentroCusto());
+        expResult.add(new CentroCusto());
+        expResult.add(new CentroCusto());
+        expResult.add(new CentroCusto());
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+
+        // apaga tudo
+        instance.findCentroCustoEntities().stream().forEach((centroCusto) -> {
+            try {
+                instance.destroy(centroCusto.getId());
+            } catch (NonexistentEntityException ex) {
+                fail("Exceção não esperada");
+            }
+        });
+
+        expResult.stream().forEach((centroCusto) -> {
+            instance.create(centroCusto);
+        });
+
         List<CentroCusto> result = instance.findCentroCustoEntities();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        assertArrayEquals(expResult.toArray(), result.toArray());
+
+        try {
+
+            for (CentroCusto centroCusto : result) {
+                instance.destroy(centroCusto.getId());
+
+            }
+
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
     }
 
     /**
@@ -113,14 +209,60 @@ public class CentroCustoDAOTest {
     @Test
     public void testFindCentroCustoEntities_int_int() {
         System.out.println("findCentroCustoEntities");
-        int maxResults = 0;
-        int firstResult = 0;
-        CentroCustoDAO instance = null;
-        List<CentroCusto> expResult = null;
+
+        int maxResults = 2;
+        int firstResult = 1;
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+
+        // apaga tudo
+        instance.findCentroCustoEntities().stream().forEach((centroCusto) -> {
+            try {
+                instance.destroy(centroCusto.getId());
+            } catch (NonexistentEntityException ex) {
+                fail("Exceção não esperada");
+            }
+        });
+
+        // apaga tudo
+        instance.findCentroCustoEntities().stream().forEach((centroCusto) -> {
+            try {
+                instance.destroy(centroCusto.getId());
+            } catch (NonexistentEntityException ex) {
+                fail("Exceção não esperada");
+            }
+        });
+
+        List<CentroCusto> allElements = new ArrayList<>();
+        allElements.add(new CentroCusto());
+        allElements.add(new CentroCusto());
+        allElements.add(new CentroCusto());
+        allElements.add(new CentroCusto());
+        allElements.add(new CentroCusto());
+
+        allElements.stream().forEach((centroCusto) -> {
+            instance.create(centroCusto);
+        });
+
+        CentroCusto[] expResult = new CentroCusto[maxResults];
+        expResult[0] = allElements.get(1);
+        expResult[1] = allElements.get(2);
+
         List<CentroCusto> result = instance.findCentroCustoEntities(maxResults, firstResult);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        assertArrayEquals(expResult, result.toArray());
+
+        try {
+
+            for (CentroCusto centroCusto : allElements) {
+                instance.destroy(centroCusto.getId());
+
+            }
+
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
     }
 
     /**
@@ -129,13 +271,24 @@ public class CentroCustoDAOTest {
     @Test
     public void testFindCentroCusto() {
         System.out.println("findCentroCusto");
-        Integer id = null;
-        CentroCustoDAO instance = null;
-        CentroCusto expResult = null;
-        CentroCusto result = instance.findCentroCusto(id);
+
+        CentroCusto expResult = new CentroCusto();
+        expResult.setCentroCusto("NomeCC");
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+        instance.create(expResult);
+
+        CentroCusto result = instance.findCentroCusto(expResult.getId());
+
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        try {
+            instance.destroy(result.getId());
+
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
     }
 
     /**
@@ -144,12 +297,29 @@ public class CentroCustoDAOTest {
     @Test
     public void testGetCentroCustoCount() {
         System.out.println("getCentroCustoCount");
-        CentroCustoDAO instance = null;
-        int expResult = 0;
+
+        CentroCustoDAO instance = new CentroCustoDAO(PostgresConnection.getInstance().getEntityManagerFactory());
+
+        // apaga tudo
+        try {
+            for (CentroCusto centroCusto : instance.findCentroCustoEntities()) {
+                instance.destroy(centroCusto.getId());
+            }
+        } catch (NonexistentEntityException ex) {
+            fail("Exceção não esperada");
+        }
+
+        int expResult = 5;
+
+        instance.create(new CentroCusto());
+        instance.create(new CentroCusto());
+        instance.create(new CentroCusto());
+        instance.create(new CentroCusto());
+        instance.create(new CentroCusto());
+
         int result = instance.getCentroCustoCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
-    
+
 }
